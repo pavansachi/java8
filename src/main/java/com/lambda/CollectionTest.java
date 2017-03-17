356869725d8ca3dc9ce813f55b9b3d0ba9b34584
pavansachi <pavan8sachi@gmail.com>
2017-03-18 09:34:52 +1100

    lambda collections

---
+++ src/main/java/com/lambda/CollectionTest.java
@@ -0,0 +1,27 @@
+package com.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionTest {
+
+	public static void main(String[] args) {
+
+		List<String> engines = Arrays.asList("google", "yahoo", "bing");
+		
+		Collections.sort(engines, (a, b) -> {
+			return a.compareTo(b);
+		} );
+		
+		System.out.format("Search engines asc: %s\n", engines);
+		
+		Collections.sort(engines, (a, b) -> {
+			return b.compareTo(a);
+		} );
+		
+		System.out.format("Search engines desc: %s", engines);
+		
+	}
+
+}
